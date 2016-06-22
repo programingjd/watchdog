b853c354db7f4096d986764fdda39cb462a95380
DAVID, Jerome <programingjd@gmail.com>
2016-06-22 10:50:18 +0200

    Create watchdog.sh

---
+++ watchdog.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# defaults to my_service is none is specified
+SERVICE=${1:-"my_service"}
+echo $SERVICE
+
+WATCHDOG_FILE="~/watchdog-$SERVICE"
+touch $WATCHDOG_FILE
+
+(
+  flock -x -w 3 200 || exit 1
+  while true; do
+    /etc/init.d/$SERVICE status  || /etc/init.d/$SERVICE start
+    sleep 3
+  done
+) 200>$WATCHDOG_FILE
