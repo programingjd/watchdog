22038ea61220d139d98b3c379124fcee6540789e
DAVID, Jerome <programingjd@gmail.com>
2016-06-22 11:04:04 +0200

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -2,15 +2,15 @@
 
 # defaults to my_service is none is specified
 SERVICE=${1:-"my_service"}
-echo $SERVICE
+echo ${SERVICE}
 
-WATCHDOG_FILE="${HOME}/watchdog-$SERVICE"
-touch $WATCHDOG_FILE
+WATCHDOG_FILE="${HOME}/watchdog-${SERVICE}"
+touch "${WATCHDOG_FILE}"
 
 (
   flock -x -w 3 200 || exit 1
   while true; do
-    /etc/init.d/$SERVICE status  || /etc/init.d/$SERVICE start
+    "/etc/init.d/${SERVICE}" status  || "/etc/init.d/${SERVICE}" start
     sleep 3
   done
-) 200>$WATCHDOG_FILE
+) 200>"${WATCHDOG_FILE}"
